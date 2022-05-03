bb69a2c7a123929837df7ad858e556e130756b6d
ALagutin <jXgWpySgnc9T5ZJ>
2022-05-03 19:39:24 +0300

    spring init

---
+++ src/test/java/ru/stencom/restaurantvoting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stencom.restaurantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
