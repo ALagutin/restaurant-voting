8cf9b17a846957cadbf04c4bd76e11020b1b1b95
ALagutin <jXgWpySgnc9T5ZJ>
2022-05-10 17:44:17 +0300

    create h2-base with some test data

---
+++ src/main/java/ru/stencom/restaurantvoting/config/AppConfig.java
@@ -0,0 +1,16 @@
+package ru.stencom.restaurantvoting.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+}
