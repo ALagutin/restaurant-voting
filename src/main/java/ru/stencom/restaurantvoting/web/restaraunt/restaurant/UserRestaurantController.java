5b5887e6b849e568bc3b58a934221f940c80594e
ALagutin <jXgWpySgnc9T5ZJ>
2022-05-14 16:08:34 +0300

    add UserRestaurantController

---
+++ src/main/java/ru/stencom/restaurantvoting/web/restaraunt/restaurant/UserRestaurantController.java
@@ -0,0 +1,36 @@
+package ru.stencom.restaurantvoting.web.restaraunt.restaurant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.stencom.restaurantvoting.model.Restaurant;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = UserRestaurantController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserRestaurantController extends AbstractRestaurantController{
+
+    static final String REST_URL = "/api/restaurants";
+
+    @GetMapping("/with-menu")
+    public List<Restaurant> getAllWithMenu() {
+        return super.getAllWithMenu();
+    }
+
+    @GetMapping("/{id}/with-menu")
+    public Restaurant getWithMenu(@PathVariable int id) {
+        return super.getWithMenu(id);
+    }
+
+    @GetMapping
+    public List<Restaurant> getAll() {
+        return super.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Restaurant get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+}
