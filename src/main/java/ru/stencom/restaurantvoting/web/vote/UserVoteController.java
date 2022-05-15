7cfb4b49ba913b4c7ab8fb128e501d64827c22e4
ALagutin <jXgWpySgnc9T5ZJ>
2022-05-15 19:58:35 +0300

    fix TODO in user vote controller

--- src/main/java/ru/stencom/restaurantvoting/web/vote/UserVoteController.java
+++ src/main/java/ru/stencom/restaurantvoting/web/vote/UserVoteController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.stencom.restaurantvoting.error.IllegalRequestDataException;
@@ -12,6 +13,7 @@ import ru.stencom.restaurantvoting.to.VoteTo;
 import ru.stencom.restaurantvoting.repository.VoteRepository;
 import ru.stencom.restaurantvoting.service.VoteService;
 import ru.stencom.restaurantvoting.util.to.VoteUtil;
+import ru.stencom.restaurantvoting.web.AuthUser;
 
 import java.net.URI;
 import java.util.List;
@@ -32,10 +34,8 @@ public class UserVoteController {
 
 
     @PostMapping
-    public ResponseEntity<ru.stencom.restaurantvoting.to.VoteTo> createWithLocation(@RequestParam int restaurantId) {
-        //TODO replace USER_ID after impl auth
-        int userId = USER_ID;
-
+    public ResponseEntity<ru.stencom.restaurantvoting.to.VoteTo> createWithLocation(@AuthenticationPrincipal AuthUser authUser, @RequestParam int restaurantId) {
+        int userId = authUser.id();
         log.info("create vote for restaurant id={} user id={}", userId, restaurantId);
         VoteTo created = service.save(userId, restaurantId);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
@@ -45,23 +45,20 @@ public class UserVoteController {
     }
 
     @GetMapping
-    public VoteTo get() {
-        //TODO replace USER_ID after impl auth
-        int userId = USER_ID;
-
+    public VoteTo get(@AuthenticationPrincipal AuthUser authUser) {
+        int userId = authUser.id();
         Optional<Vote> vote = repository.get(userId);
 
         if (vote.isPresent()) {
             return VoteUtil.createTo(vote.get());
         } else {
-            throw new IllegalRequestDataException("Vote for today is not exist.");
+            throw new IllegalRequestDataException("today's vote is not exist.");
         }
     }
 
     @GetMapping("/all")
-    public List<VoteTo> getAll() {
-        //TODO replace USER_ID after impl auth
-        int userId = USER_ID;
+    public List<VoteTo> getAll(@AuthenticationPrincipal AuthUser authUser) {
+        int userId = authUser.id();
         return VoteUtil.getTos(repository.getAll(userId));
     }
 
